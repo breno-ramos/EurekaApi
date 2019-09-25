344142ea582c79c1122578644f137d7941fe1de5
Marllon Ramos <87marllon@gmail.com>
2019-09-25 12:35:27 -0300

    Creation of the branch domain

---
+++ Eureka.Domain/Services/IBranchApplicationService.cs
@@ -0,0 +1,16 @@
+﻿using Eureka.Domain.Commands.BranchCommands;
+using Eureka.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Eureka.Domain.Services
+{
+    public interface IBranchApplicationService
+    {
+        List<Branch> Get(int skip, int take);
+        Branch GetById(Guid id);
+        Branch Create(CreateBranchCommand branch);
+        Branch Update(CreateBranchCommand branch);
+        Branch Delete(Guid id);
+    }
+}
