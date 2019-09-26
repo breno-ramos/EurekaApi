29eba48c15d27d5b95f44d0774f4133e1b1a0c13
Breno Ramos <ramosb317@hotmail.com>
2019-09-25 21:57:00 -0300

    Creation of the plan domain

---
+++ Eureka.Domain/Entities/Plan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Eureka.Domain.Shared.Entities;
+using Eureka.Domain.Shared.Enums;
+using Eureka.Domain.Scopes;
+
+namespace Eureka.Domain.Entities
+{
+    public class Plan : Entity
+    {
+        public Plan(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            CreateDate = DateTime.Now;
+            Status = EStatusType.Active;
+
+            this.ValidName();
+            this.ValidDescription();
+            this.ValidPrice();
+        }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public DateTime CreateDate { get; private set; }
+        public EStatusType Status { get; private set; }
+    }
+}
---
+++ Eureka.Domain/Scopes/PlanScope.cs
@@ -0,0 +1,31 @@
+﻿using Eureka.Domain.Entities;
+using Eureka.SharedKernel.Validation;
+
+namespace Eureka.Domain.Scopes
+{
+    public static class PlanScope
+    {
+        public static bool ValidName(this Plan plan)
+        {
+            return AssertionConcern.IsValid(
+                AssertionConcern.AssertArgumentLength(plan.Name, 3, 80, "Nome do plano deve possuir mais que 2 até 80 caracteres."),
+                AssertionConcern.AssertArgumentNotEmpty(plan.Name, "Informe o nome do plano.")
+                );
+        }
+
+        public static bool ValidDescription(this Plan plan)
+        {
+            return AssertionConcern.IsValid(
+                    AssertionConcern.AssertArgumentLength(plan.Description, 10, 150, "Descrição do plano deve possuir mais do que 9 até 150 caracteres."),
+                    AssertionConcern.AssertArgumentNotEmpty(plan.Description, "Informe uma descrição para o plano.")
+                );
+        }
+
+        public static bool ValidPrice(this Plan plan)
+        {           
+          return  AssertionConcern.IsValid(
+                AssertionConcern.AssertArgumentNotEquals(plan.Price, 0, "Preço do plano não pode ser igual a 0.")
+                );
+        }
+    }
+}
