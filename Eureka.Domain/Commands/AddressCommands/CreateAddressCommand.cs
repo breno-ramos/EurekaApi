e538ed4c70c898d512275a1fcb2b7066edd4591c
Marllon Ramos <87marllon@gmail.com>
2019-09-27 06:37:52 -0300

    Creation of the address domain and update validation

---
+++ Eureka.Domain/Commands/AddressCommands/CreateAddressCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Eureka.Domain.Commands.AddressCommands
+{
+    public class CreateAddressCommand
+    {
+        public int Number { get; set; }
+        public string Street { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Complement { get; set; }
+    }
+}
