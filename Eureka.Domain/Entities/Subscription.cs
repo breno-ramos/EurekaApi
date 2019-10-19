0d79264b82fd5ec87ca92ef55fba0c82fe6ab81a
Breno Ramos <ramosb317@hotmail.com>
2019-10-19 12:20:49 -0300

    Creation of the subscription map, subscription repository implementation and relationship correction

--- Eureka.Domain/Entities/Subscription.cs
+++ Eureka.Domain/Entities/Subscription.cs
@@ -6,10 +6,9 @@ namespace Eureka.Domain.Entities
 {
     public class Subscription : Entity
     {
-        public Subscription(ESubscriptionType type, Customer customer, Plan plan)
+        public Subscription(ESubscriptionType type, Plan plan)
         {
             Type = type;
-            Customer = customer;
             Plan = plan;
             CreateIn = DateTime.Now;
             ExpiresIn = DateTime.Now.AddYears(1);
@@ -19,8 +18,7 @@ namespace Eureka.Domain.Entities
         public DateTime CreateIn { get; private set; }
         public DateTime ExpiresIn { get; private set; }
         public EStatusType Status { get; private set; }
-        public ESubscriptionType Type { get; private set; }
-        public Customer Customer { get; private set; }
+        public ESubscriptionType Type { get; private set; }    
         public Plan Plan { get; private set; }
     }
 }
---
+++ Eureka.Infra/Persistence/Maps/SubscriptionMap.cs
@@ -0,0 +1,30 @@
+﻿using Eureka.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Eureka.Infra.Persistence.Maps
+{
+    public class SubscriptionMap : EntityTypeConfiguration<Subscription>
+    {
+        public SubscriptionMap()
+        {
+            ToTable("Subscription");
+
+            Property(x => x.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.CreateIn)
+                .IsRequired();
+
+            Property(x => x.ExpiresIn);
+
+            Property(x => x.Status)
+                .IsRequired();
+
+            Property(x => x.Type)
+                .IsRequired();
+
+            HasRequired(x => x.Plan);
+        }
+    }
+}
