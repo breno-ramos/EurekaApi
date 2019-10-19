2bcebbb6e19294aae257bfe735b7e03ac74307e0
Marllon Ramos <87marllon@gmail.com>
2019-10-19 11:40:35 -0300

    Creation of the address repository and correction address repository interface

---
+++ Eureka.Infra/Repositories/AddressRepository.cs
@@ -0,0 +1,32 @@
+﻿using Eureka.Domain.Entities;
+using Eureka.Domain.Repositories;
+using Eureka.Infra.Persistence.DataContexts;
+using System.Data.Entity;
+
+namespace Eureka.Infra.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private EurekaDataContext _context;
+
+        public AddressRepository(EurekaDataContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Address address)
+        {
+            _context.Addresses.Add(address);
+        }
+
+        public void Delete(Address address)
+        {
+            _context.Addresses.Remove(address);
+        }
+
+        public void Update(Address address)
+        {
+            _context.Entry<Address>(address).State = EntityState.Modified;
+        }
+    }
+}
