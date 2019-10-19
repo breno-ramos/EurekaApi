47a781379c10040ac19702092ef6b7121650dd3c
Marllon Ramos <87marllon@gmail.com>
2019-10-19 11:20:54 -0300

    Creation of the model domain

---
+++ Eureka.Domain/Repositories/IModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Eureka.Domain.Entities;
+
+namespace Eureka.Domain.Repositories
+{
+    public interface IModelRepository
+    {
+        List<Model> GetModelByBrandId(Guid id);
+        void Create(Model model);
+        void Update(Model model);
+        void Delete(Model model);
+    }
+}
---
+++ Eureka.Domain/Scopes/ModelScope.cs
@@ -0,0 +1,16 @@
+﻿using Eureka.Domain.Entities;
+using Eureka.SharedKernel.Validation;
+
+namespace Eureka.Domain.Scopes
+{
+    public static class ModelScope
+    {
+        public static bool ValidName(this Model model)
+        {
+            return AssertionConcern.IsValid(
+                AssertionConcern.AssertArgumentLength(model.Nome, 3, 50, "Modelo deve possuir mais que 3 até 50 caracteres."),
+                AssertionConcern.AssertArgumentNotEmpty(model.Nome, "Informe o modelo.")
+            );
+        }
+    }
+}
